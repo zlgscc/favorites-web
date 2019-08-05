94cb26bf4d65d626d7e9c7952cd98fd039f8774b
zhs0311001 <zhs0311001@163.com>
2019-08-05 20:40:35 +0800

    添加子线程，并使用Callable接口进行回调

--- app/src/main/java/com/favorites/pool/FutureTask.java
+++ app/src/main/java/com/favorites/pool/FutureTask.java
@@ -1,4 +1,18 @@
 package com.favorites.pool;
 
-public class FutureTask {
+import java.util.concurrent.Callable;
+
+public class FutureTask implements Callable<Integer> {
+
+
+    public Integer call() throws Exception{
+        System.out.println("子线程在进行计算");
+        Thread.sleep(3000);
+        int sum = 0;
+        for(int i=0;i<100;i++)
+            sum += i;
+        return sum;
+    }
+
+
 }
